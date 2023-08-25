c7883d0a4ab251e21ed1e3292074a3d137f4535e
Edyt Daulay <chris.stone97@gmail.com>
2023-08-25 17:18:08 +0700

    add insert offering + move to offer

---
+++ job-portal-fe/projects/admin/src/app/dto/offer/offering.insert.req.dto.ts
@@ -0,0 +1,4 @@
+export interface OfferingInsertReqDto{
+    applicantId: string
+    offerSalary: number
+}
---
+++ job-portal-fe/projects/admin/src/app/dto/offer/offering.res.dto.ts
@@ -0,0 +1,7 @@
+export interface OfferingResDto{
+    id : string
+    applicantId : string
+    offerTime : string
+    offerPicName : string
+    offerSalary : number
+}
